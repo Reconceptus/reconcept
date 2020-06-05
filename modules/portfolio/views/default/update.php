789861a3b547fb09fdd09b10900ef6ae50cd759c
venodon <venodon@gmail.com>
2020-06-05 16:11:01 +0300

    gallery background

--- modules/portfolio/views/default/update.php
+++ modules/portfolio/views/default/update.php
@@ -1,11 +1,13 @@
 <?php
 
+use modules\utils\models\UtilsGallery;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $tags array */
 /* @var $hiddenTags array */
 /* @var $model modules\portfolio\models\Portfolio */
+/* @var $galleries UtilsGallery[] */
 
 $this->title = $model->isNewRecord ? 'Добавить портфолио' : 'Изменить портфолио: ' . $model->name;
 ?>
@@ -17,6 +19,7 @@ $this->title = $model->isNewRecord ? 'Добавить портфолио' : 'И
         'model'      => $model,
         'tags'       => $tags,
         'hiddenTags' => $hiddenTags,
+        'galleries'  => $galleries
     ]) ?>
 
 </div>
