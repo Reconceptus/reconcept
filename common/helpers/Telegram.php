8ed0a61ecc6782f6b2c1b1d98581776c679ec992
venodon <venodon@gmail.com>
2020-08-25 17:35:54 +0300

    telegram

--- common/helpers/Telegram.php
+++ common/helpers/Telegram.php
@@ -1,32 +1,58 @@
 <?php
 
-
 namespace common\helpers;
 
-
 use Yii;
-use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
-use yii\httpclient\Client;
 
 class Telegram
 {
-    public static function send($text, $email = null, $params = [])
+    public static function send($text, $user = null)
     {
-        if (!$email) {
-            $email = Yii::$app->params['adminEmail'];
+        $telegramId = self::getAddress($user);
+        if ($telegramId) {
+            if (!is_string($text)) {
+                $text = Json::encode($text);
+            }
+            if (mb_strlen($text) > 4090) {
+                $text = mb_substr($text, 0, 4090);
+            }
+            Yii::$app->telegram->sendMessage([
+                'chat_id' => $telegramId,
+                'text'    => $text
+            ]);
         }
-        $client = new Client();
-        $response = $client->createRequest()
-            ->setMethod('POST')
-            ->setFormat(Client::FORMAT_JSON)
-            ->setUrl(Yii::$app->params['telegramUrl'])
-            ->setData([
-                'email' => $email, 'text' => $text, 'site' => ArrayHelper::getValue(Yii::$app->params, 'telegramBearer') ?? 'reconcept.ru'
-            ])
-            ->send();
-        if (!$response->isOk) {
-            Yii::info('Проблема телеграма:'.Json::encode($response));
+    }
+
+
+    public static function sendFile($path, $user = null)
+    {
+        $telegramId = self::getAddress($user);
+        if ($telegramId) {
+            Yii::$app->telegram->sendDocument([
+                'chat_id'  => $telegramId,
+                'caption'  => Yii::$app->name,
+                'document' => $path
+            ]);
         }
+        return 1;
+    }
+
+
+    public static function getAddress($user)
+    {
+        if ($user && $user->auth && $user->auth->telegram) {
+            $telegramId = $user->auth->telegram;
+        } else {
+            $telegramId = Yii::$app->params['techTelegram'];
+            if (!$telegramId) {
+                $telegramId = '245375279';
+            }
+        }
+        if ($telegramId) {
+            return $telegramId;
+        }
+        Yii::info('There is no telegramId for user '.$user->email);
+        return false;
     }
 }
